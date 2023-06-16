583c6b091d5adcda8936894f5d2abbc3516a873c
bestmycode <bestmycode610@gmail.com>
2023-06-16 14:09:31 +0800

    init app

---
+++ Classiq Kiosk/Classiq_KioskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Classiq_KioskApp.swift
+//  Classiq Kiosk
+//
+//  Created by MAC on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Classiq_KioskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Classiq KioskUITests/Classiq_KioskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Classiq_KioskUITestsLaunchTests.swift
+//  Classiq KioskUITests
+//
+//  Created by MAC on 6/16/23.
+//
+
+import XCTest
+
+final class Classiq_KioskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
